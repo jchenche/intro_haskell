39b2a0754c67672ff39cd6c89d8ceb39442bff5d
Jimmy Chen Chen <jimmy.chenchen@irdeto.com>
2021-06-20 09:59:24 -0700

    minor refactor

--- hw6 - Lazy evaluation/Fibonacci.hs
+++ hw6 - Lazy evaluation/Fibonacci.hs
@@ -12,7 +12,9 @@ fibs1 = map fib [0..]
 
 -- Exercise 2
 fibs2 :: [Integer]
-fibs2 = 0 : (map last $ iterate (\fibs -> let l = length fibs in fibs ++ [fibs!!(l-1) + fibs!!(l-2)]) [0, 1])
+-- fibs2 = 0 : (map last $ iterate (\fibs -> let l = length fibs in fibs ++ [fibs!!(l-1) + fibs!!(l-2)]) [0, 1])
+fibs2 = f 0 1
+    where f a b = a : f b (a+b)
 
 -- Exercise 3
 data Stream a = Cons' a (Stream a)
@@ -21,7 +23,7 @@ streamToList :: Stream a -> [a]
 streamToList (Cons' e s) = e : streamToList s
 
 instance Show a => Show (Stream a) where
-    show s = show (take 20 (streamToList s))
+    show = show . take 20 . streamToList
 
 -- Exercise 4
 streamRepeat :: a -> Stream a
@@ -31,7 +33,7 @@ streamMap :: (a -> b) -> Stream a -> Stream b
 streamMap f (Cons' e s) = Cons' (f e) (streamMap f s)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed getNewSeed seed = Cons' seed (streamFromSeed getNewSeed (getNewSeed seed))
+streamFromSeed f seed = Cons' seed (streamFromSeed f (f seed))
 
 -- Exercise 5
 nats :: Stream Integer
@@ -50,14 +52,11 @@ largestPowerOfTwoThatDividesN (prevN, _) =
 -- ruler :: Stream Integer
 -- ruler = streamMap snd $ streamFromSeed largestPowerOfTwoThatDividesN (1, 0)
 
+-- visualize: interleaveStreams (streamRepeat 0) (interleaveStreams (streamRepeat 1) (interleaveStreams (streamRepeat 2) (interleaveStreams ...
 ruler :: Stream Integer
--- ruler = interleaveStreams (streamRepeat 0) (interleaveStreams (streamRepeat 1) (interleaveStreams (streamRepeat 2) (interleaveStreams ...
 -- ruler = interleaveStreams (streamRepeat 0) (streamMap (1+) ruler)
-ruler =
-    let
-        helper :: Integer -> Stream Integer
-        helper repeat = interleaveStreams (streamRepeat repeat) (helper (repeat+1))
-    in helper 0
+ruler = f 0
+    where f repeat = interleaveStreams (streamRepeat repeat) (f (repeat+1))
 
 -- Exercise 6
 x :: Stream Integer
@@ -77,6 +76,7 @@ fibs3 = x / (1 - x - x^2)
 
 -- Exercise 7
 data Matrix = Matrix Integer Integer Integer Integer
+
 instance Num Matrix where
     (Matrix m00 m01 m10 m11) * (Matrix n00 n01 n10 n11) =
         Matrix (m00 * n00 + m01 * n10) (m00 * n01 + m01 * n11) (m10 * n00 + m11 * n10) (m10 * n01 + m11 * n11)
