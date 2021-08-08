adcf9f90268edfa252827d0d4aae3761abaa69fd
Jimmy Chen Chen <jimmy.chenchen@irdeto.com>
2021-08-08 15:44:30 -0700

    Clean up hw12

--- hw12 - Monads/Risk.hs
+++ hw12 - Monads/Risk.hs
@@ -26,7 +26,7 @@ die = getRandom
 
 type Army = Int
 
-data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving Show
+data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
 
 -- Exercise 1
 -- Just execute cabal list --installed and read up on it
@@ -34,9 +34,9 @@ data Battlefield = Battlefield { attackers :: Army, defenders :: Army } deriving
 -- Exercise 2
 evalBattle :: [(Int, Int)] -> Battlefield -> Battlefield
 evalBattle [] bf = bf
-evalBattle ((attackerDV, defenderDV):battleDVs) (Battlefield attacker defender) = if attackerDV > defenderDV
-                                                                                  then evalBattle battleDVs (Battlefield attacker (defender - 1))
-                                                                                  else evalBattle battleDVs (Battlefield (attacker - 1) defender)
+evalBattle ((attackerDV, defenderDV):battleDVs) (Battlefield attacker defender)
+  | attackerDV > defenderDV = evalBattle battleDVs (Battlefield attacker (defender - 1))
+  | otherwise               = evalBattle battleDVs (Battlefield (attacker - 1) defender)
 
 reverseSort :: Ord a => [a] -> [a]
 reverseSort = reverse . sort
@@ -44,12 +44,15 @@ reverseSort = reverse . sort
 zipBattleDieVals :: [DieValue] -> [DieValue] -> [(Int, Int)]
 zipBattleDieVals attackerDVs defenderDVs = zip (reverseSort $ map unDV attackerDVs) (reverseSort $ map unDV defenderDVs)
 
-maxAttackerToDeploy = 3
-maxDefenderToDeploy = 2
+maxAttackers :: Army -> Int
+maxAttackers n = min 3 (n - 1)
+
+maxDefenders :: Army -> Int
+maxDefenders = min 2
 
 battle :: Battlefield -> Rand StdGen Battlefield
-battle bf@(Battlefield attacker defender) = replicateM (min maxAttackerToDeploy (attacker - 1)) die >>= \attackerDVs ->
-                                            replicateM (min maxDefenderToDeploy defender) die >>= \defenderDVs ->
+battle bf@(Battlefield attacker defender) = replicateM (maxAttackers attacker) die >>= \attackerDVs ->
+                                            replicateM (maxDefenders defender) die >>= \defenderDVs ->
                                             return $ evalBattle (zipBattleDieVals attackerDVs defenderDVs) bf
 
 -- Exercise 3
@@ -63,15 +66,14 @@ interpretWinner :: Battlefield -> Double
 interpretWinner (Battlefield 1 defender) = 0
 interpretWinner (Battlefield attacker 0) = 1
 
-successProb :: Battlefield -> Rand StdGen Double
-successProb bf = replicateM 1000 (invade bf) >>= \battleResults -> return $ (sum $ map interpretWinner battleResults) / 1000
+simulationRuns :: Int
+simulationRuns = 1000
 
--- test = (evalRandIO $ successProb (Battlefield 11 10)) >>= \x -> print x
+successProb :: Battlefield -> Rand StdGen Double
+successProb bf = replicateM simulationRuns (invade bf) >>= \battleResults -> return $ (sum $ map interpretWinner battleResults) / fromIntegral simulationRuns
+
+test = (evalRandIO $ successProb (Battlefield 11 10)) >>= print
 
 -- Exercise 5
 -- exactSuccessProb :: Battlefield -> Double
 -- exactSuccessProb (Battlefield attacker defender) = 
-
-
-
-
